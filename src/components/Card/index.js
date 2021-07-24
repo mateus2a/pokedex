d53b52849564fdf161a51fd6e49cd8b040a0aefb
RafaelL1ma <lima.rafael67@hotmail.com>
2021-07-24 20:53:55 -0300

    fix: corrections in card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export const Card = (props) => (
 <div>
-  <img alt="pokemon"src={`https://img.pokemondb.net/artwork/large/${props.pokemon.name}.jpg`}/>
-  <h2>{props.pokemon.name[0].toUpperCase() + props.pokemon.name.slice(1)}</h2>
-  <h2>{props.pokemon.type[0]}</h2>
+  <a href={`https://www.pokemon.com/us/pokedex/${props.pokemon.name}`}>
+    <img alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${props.pokemon.name}.jpg`}/>
+    <h2>{props.pokemon.name[0].toUpperCase() + props.pokemon.name.slice(1)}</h2>
+  </a>
 </div>
 );
